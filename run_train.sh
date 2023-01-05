114ff04ffa79f8c9ef2c6d6f86ca9b9365fbc23e
OrKatz7 <Or.Katz1@gmail.com>
2023-01-05 21:48:06 +0200

    Add files via upload

---
+++ run_train.sh
@@ -0,0 +1,7 @@
+cd src
+
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_baseline/
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_baseline/
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_baseline/
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v2-xlarge --outputs_dir ../outputs_baseline/
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v2-large --outputs_dir ../outputs_baseline/
