f5ab8aea7044be168790b740eb54c64ddda195f5
OrKatz7 <Or.Katz1@gmail.com>
2023-01-16 11:11:56 +0200

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,13 @@
-cd src
-python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_baseline/
-python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_baseline/
-python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_baseline/
-python main.py --model microsoft/deberta-v2-xlarge --outputs_dir ../outputs_baseline/
-python main.py --model microsoft/deberta-v2-large --outputs_dir ../outputs_baseline/
+cd ../src
+
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate 
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_baseline_reg/
+
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_baseline_reg/
+
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
+/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_baseline_reg/
