05a8d03ccccebbf280505b463f3cf8287cc58dc7
OrKatz7 <Or.Katz1@gmail.com>
2023-01-16 12:25:28 +0200

    Update run_train.sh

--- code/run_train.sh
+++ code/run_train.sh
@@ -1,13 +1,11 @@
 cd ../src
 
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate 
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_baseline_reg/
-
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_baseline_reg/
-
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
-/home/kaor/.conda/envs/mmdet/bin/python main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_baseline_reg/
+python3 main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate 
+python3 main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
+python3 main.py --model Narrativaai/deberta-v3-small-finetuned-hate_speech18 --outputs_dir ../outputs_baseline_reg/
+python3 main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate
+python3 main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
+python3 main.py --model microsoft/deberta-v3-base --outputs_dir ../outputs_baseline_reg/
+python3 main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_toxigen_backtranslate_reg/ --back_translation --pretrain_hate
+python3 main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_backtranslate_reg/ --back_translation 
+python3 main.py --model microsoft/deberta-v3-large --outputs_dir ../outputs_baseline_reg/
